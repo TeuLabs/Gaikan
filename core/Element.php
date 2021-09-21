bdc2427995a781094f361e8b6a769a1387899673
Matteu <matteugt@gmail.com>
2021-09-22 02:25:19 +0800

    Add PHPDocs

--- core/Element.php
+++ core/Element.php
@@ -10,9 +10,12 @@ class Element
     private static string $componentFolder = '\app\src\components\\';
 
     /**
+     * Renders final component
+     * @param string $component
      * @throws Exception
+     * @return mixed
      */
-    public static function render(string $component)
+    public static function render(string $component): mixed
     {
         $parsed = self::parse($component);
         $file = '../src/components/' . $parsed[0] . '.php';
@@ -21,9 +24,12 @@ class Element
     }
 
     /**
+     * Parses component for its information and puts it in an array.
+     * @param string $component
      * @throws Exception
+     * @return array
      */
-    private static function parse(string $component)
+    private static function parse(string $component): array
     {
         $element = new SimpleXMLElement($component);
         $tagName = $element->getName();
@@ -40,6 +46,12 @@ class Element
         return [$tagName, $dump];
     }
 
+    /**
+     * Handles all the props in a component. This function is responsible for
+     * sanitizing the raw SimpleXMLElement object data.
+     * @param array|object $attributes
+     * @return array
+     */
     protected static function handleProps(array|object $attributes): array
     {
         $attributeBag = [];
